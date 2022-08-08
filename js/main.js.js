992d85928c4b04e3259ec72529f8fcb4dfa8aa29
Buroffsky <110665112+Buroffsky@users.noreply.github.com>
2022-08-08 22:54:42 +0200

    finish

--- js/main.js.js
+++ js/main.js.js
@@ -1,28 +1,65 @@
-const ajaxSend = async (FormData) => {
+// const ajaxSend = async (formData) => {
+//   const fetchResp = await fetch("telegram.php", {
+//     method: "POST",
+//     body: formData,
+//   });
+//   if (!fetchResp.ok) {
+//     throw new Error(
+//       `Ошибка по адресу ${url}, статус ошибка ${fetchResp.status}`
+//     );
+//   }
+//   return await fetchResp.text();
+// };
+
+// const forms = document.querySelectorAll("form");
+// forms.forEach((form) => {
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     const formData = new FormData(this);
+//     console.log(formData);
+
+//     ajaxSend(formData)
+//       .then((response) => {
+//         this.innerHTML = "Спасибо, <br> заявку получили";
+//         form.reset();
+//       })
+//       .catch((err) => console.error(err));
+//   });
+// });
+"use strict";
+const ajaxSend = async (formData) => {
+  // создаем функцию отправки формы
   const fetchResp = await fetch("telegram.php", {
-    method: "POST",
-    body: formData,
+    // указываем обработчик формы — telegram.php
+    method: "POST", // метод, которым мы отправляем форму
+    body: formData, // что будет внутри формы — содержимое input
   });
   if (!fetchResp.ok) {
+    // если ошибка, то...
     throw new Error(
-      `Ошибка по адресу ${url}, статус ошибка ${fetchResp.status}`
-    );
+      `Ошибка по адресу ${url}, статус ошибки ${fetchResp.status}`
+    ); // выводим статус ошибки и текст
   }
-  return await fetchResp.text();
+  return await fetchResp.text(); // если все хорошо, возвращаем ответ сервера
 };
 
-const forms = document.querySelectorAll("form");
+const forms = document.querySelectorAll("form"); // находим все теги form
 forms.forEach((form) => {
+  // для каждой формы...
   form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const formData = new FormData(this);
+    // отслеживаем событие отправки
+    e.preventDefault(); // отменить стандартную отправку формы
+
+    const formData = new FormData(this); // собираем все данные из формы
     console.log(formData);
 
-    ajaxSend(formData)
+    ajaxSend(formData) // передаем данные из формы в обработчик
       .then((response) => {
-        this.innerHTML = "Спасибо, <br> заявку получили";
-        form.reset();
+        // если все успешно, то..
+        this.innerHTML =
+          "Спасибо,<br> заявку получили"; /* окно благодарности */
+        form.reset(); /*  очищаем поля формы */
       })
-      .catch((err) => console.error(err));
+      .catch((err) => console.error(err)); /* если ошибка, выводим в консоль */
   });
 });
